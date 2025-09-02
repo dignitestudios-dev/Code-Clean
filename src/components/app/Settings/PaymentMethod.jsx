22cc548598022a3705c66bbc9fffe38b5ed086e3
zackcole <183650880+zackcole@users.noreply.github.com>
2025-09-01 23:54:37 -0700

    update

--- src/components/app/Settings/PaymentMethod.jsx
+++ src/components/app/Settings/PaymentMethod.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../layout/Navbar";
 import { FaArrowLeft } from "react-icons/fa";
-import { RiEdit2Fill } from "react-icons/ri";
+import { RiEdit2Fill, RiLoader5Line } from "react-icons/ri";
 import { LuTrash2 } from "react-icons/lu";
 import { HeroBg, stripe } from "../../../assets/export";
 import { useNavigate } from "react-router";
 import { Button } from "../../global/GlobalButton";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  DeletePaymentMethod,
+  getPaymentMethod,
+} from "../../../redux/slices/provider.slice";
 
 export default function PaymentMethod() {
   const navigate = useNavigate("");
+  const dispatch = useDispatch();
+  const { isLoading, paymentMethod, bookingRequestLoader } = useSelector(
+    (state) => state.provider
+  );
+  useEffect(() => {
+    dispatch(getPaymentMethod());
+  }, []);
+
+  const handleDelete = (id) => {
+    dispatch(DeletePaymentMethod(id));
+  };
+
+  console.log(paymentMethod);
   return (
     <div>
       <Navbar />
@@ -20,14 +38,12 @@ export default function PaymentMethod() {
       ></div>
       <div className="h-full px-10 lg:px-40   -mt-60 bottom-0 items-center gap-3 ">
         <div className="flex items-center gap-2 mb-6">
-          <div>
-            <button type="button" onClick={() => navigate("/app/settings")}>
-              <FaArrowLeft color="white" size={16} />
-            </button>
-            <h1 className="text-[32px] font-semibold text-white">
-              Payment Method
-            </h1>
-          </div>
+          <button type="button" onClick={() => navigate("/app/settings")}>
+            <FaArrowLeft color="white" size={16} />
+          </button>
+          <h1 className="text-[32px] font-semibold text-white">
+            Payment Method
+          </h1>
         </div>
         <div className="bg-[#F9FAFA] shadow-lg flex h-[300px] flex-col gap-3 mb-48 rounded-[8px] p-10 mt-3">
           <div className="flex justify-between items-center">
@@ -42,36 +58,60 @@ export default function PaymentMethod() {
             </div>
           </div>
           <p className="text-[16px] text-[#212935] mb-1">Attached Stripe</p>
-          <div className="bg-[#FFFFFF] p-4 rounded-lg">
-            <div className="flex items-center justify-between">
-              <div className="flex items-center space-x-4">
-                <div>
-                  <div className="flex items-center space-x-3">
-                    <span className="text-gray-900 font-mono text-base">
-                      **** **** **** **72
-                    </span>
-                    <img src={stripe} className="w-[40px]" alt="" srcset="" />
+          {paymentMethod?.payment_methods &&
+          paymentMethod.payment_methods.length > 0 ? (
+            paymentMethod.payment_methods.map((item) => (
+              <div key={item?.id} className="bg-[#FFFFFF] p-4 rounded-lg">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-3">
+                      <span className="text-gray-900 font-mono text-base">
+                        **** **** **** {item?.last_digits}
+                      </span>
+                      <img
+                        src={stripe}
+                        className="w-[40px]"
+                        alt="Stripe logo"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center space-x-2">
+                    <button
+                      onClick={() => navigate("/app/edit-card")}
+                      style={{
+                        background:
+                          "linear-gradient(234.85deg, #27A8E2 -20.45%, #00034A 124.53%)",
+                      }}
+                      className="p-2 rounded-[8px] text-white transition-colors"
+                    >
+                      <RiEdit2Fill size={16} />
+                    </button>
+
+                    <button
+                      onClick={() => handleDelete(item?.id)}
+                      className="p-2 bg-red-600 text-white rounded hover:bg-red-700 transition-colors"
+                    >
+                      <div className="flex items-center">
+                        <LuTrash2 size={16} className="mr-1" />
+                        {bookingRequestLoader === item?.id && (
+                          <RiLoader5Line className="animate-spin text-lg" />
+                        )}
+                      </div>
+                    </button>
                   </div>
                 </div>
               </div>
-
-              <div className="flex items-center space-x-2">
-                <button
-                  onClick={() => navigate("/app/edit-card")}
-                  style={{
-                    background:
-                      " linear-gradient(234.85deg, #27A8E2 -20.45%, #00034A 124.53%)",
-                  }}
-                  className="p-2 rounded-[8px] text-white transition-colors"
-                >
-                  <RiEdit2Fill size={16} />
-                </button>
-                <button className="p-2 bg-red-600 text-white rounded hover:bg-red-700 transition-colors">
-                  <LuTrash2 size={16} />
-                </button>
+            ))
+          ) : (
+            <div className="bg-[#FFFFFF] p-4 rounded-lg">
+              <div className="flex items-center">
+                <span className="text-gray-900 font-mono text-base">
+                  No cards found
+                </span>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
--- src/redux/slices/provider.slice.jsx
+++ src/redux/slices/provider.slice.jsx
@@ -14,6 +14,7 @@ const initialState = {
   getprofileloading: false,
   bookingRequest: null,
   bookingRequestLoader: false,
+  paymentMethod: null,
 };
 // ================= THUNKS =================
 
@@ -93,7 +94,7 @@ export const AddCard = createAsyncThunk(
   "/provider/payment-methods",
   async (payload, thunkAPI) => {
     try {
-      const response = await axios.post("/provider/payment-methods", payload);
+      const response = await axios.post("/user/payment-methods", payload);
       SuccessToast(response?.data?.message);
       return { success: true, message: response?.data?.message };
     } catch (error) {
@@ -147,7 +148,42 @@ export const MarkStartJob = createAsyncThunk(
   "/bookings/1/action",
   async (payload, thunkAPI) => {
     try {
-      const response = await axios.post(`/bookings/${payload?.id}/action`,{action:payload?.action});
+      const response = await axios.post(`/bookings/${payload?.id}/action`, {
+        action: payload?.action,
+      });
+      SuccessToast(response?.data?.message);
+      return { success: true, message: response?.data?.message };
+    } catch (error) {
+      ErrorToast(
+        error.response?.data?.message || "Booking Request Accept failed"
+      );
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Booking Request Accept failed"
+      );
+    }
+  }
+);
+export const getPaymentMethod = createAsyncThunk(
+  "/provider/payment-methods/get-card", // The action type
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/provider/payment-methods"); // API request to fetch the profile
+      return response.data; // Assuming the API returns the user profile data
+    } catch (error) {
+      const msg = error?.response?.data?.message || "Failed to Fetch Plans";
+      ErrorToast(msg); // Show error toast
+      return thunkAPI.rejectWithValue(msg); // Handle rejection
+    }
+  }
+);
+
+export const DeletePaymentMethod = createAsyncThunk(
+  "/provider/payment-method/{payment_method_id}",
+  async (payload, thunkAPI) => {
+    try {
+      const response = await axios.delete(
+        `/provider/payment-method/${payload}`
+      );
       SuccessToast(response?.data?.message);
       return { success: true, message: response?.data?.message };
     } catch (error) {
@@ -200,10 +236,25 @@ const providerSlice = createSlice({
       })
       .addCase(getPlans.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.plans = action.payload;
+        state.plans = action.payload?.payment_methods;
         state.success = "Plans Get Successfully";
       })
       .addCase(getPlans.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      .addCase(getPaymentMethod.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+        state.success = null;
+      })
+      .addCase(getPaymentMethod.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.paymentMethod = action.payload;
+        state.success = "Payment Method Get Successfully";
+      })
+      .addCase(getPaymentMethod.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       })
@@ -243,7 +294,7 @@ const providerSlice = createSlice({
         state.success = null;
       })
       .addCase(getRequestDetail.fulfilled, (state, action) => {
-        console.log(action.payload,"payload")
+        console.log(action.payload, "payload");
         state.isLoading = false;
         state.bookingRequestDetail = action.payload;
         state.success = "Booking Request Detail Get Successfully";
@@ -264,6 +315,19 @@ const providerSlice = createSlice({
       })
       .addCase(AddCard.rejected, (state, action) => {
         state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(DeletePaymentMethod.pending, (state) => {
+        state.bookingRequestLoader = true;
+        state.error = null;
+        state.success = null;
+      })
+      .addCase(DeletePaymentMethod.fulfilled, (state, action) => {
+        state.bookingRequestLoader = false;
+        state.success = "Delete Successfully";
+      })
+      .addCase(DeletePaymentMethod.rejected, (state, action) => {
+        state.bookingRequestLoader = false;
         state.error = action.payload;
       })
       //Accept Booking Req
